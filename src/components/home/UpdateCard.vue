69d20ca23636ea8def87f8fd683e4b62f959157c
shichiya <1071342037@qq.com>
2023-07-05 11:43:24 +0800

    init

---
+++ src/components/home/UpdateCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span style="font-weight: bold;font-size: 20px">最近更新</span>
+      <!--<el-button style="float: right; padding: 3px 0" type="text">修改</el-button>-->
+    </div>
+    <div class="block">
+      <el-timeline>
+        <el-timeline-item timestamp="2023-07-05" placement="top">
+          <el-card>
+            <h4>前端想项目创建</h4>
+            <p>ShiChiYa 提交于 2023-07-05 </p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'UpdateCard'
+}
+</script>
+
+<style scoped>
+  .box-card {
+    margin-top: 5px;
+    width: 988px;
+    height: 1320px;
+    text-align: left;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 导入编写的组件
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import Home from '@/components/Home'
+import AppIndex from '@/components/home/AppIndex'
+import LibraryIndex from '@/components/library/LibraryIndex'
+import AdminIndex from '@/components/admin/AdminIndex'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/admin',
+          name: 'Admin',
+          component: AdminIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
+  ]
+})
